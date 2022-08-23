8297aed13b58bc1f4aaa19c8a62688e81952aae8
JOYCE <joychinwenduibeh93@gmail.com>
2022-08-23 22:24:53 +0100

    Added mad-lib

---
+++ app.js
@@ -0,0 +1,22 @@
+ // mainStory = Ralia is the daughter of a blind woman and a man with a broken back who goes missing in the
+// forest while arranging firewood and is taken in by an old witch who offers her accomdation 
+//in return for Ralia’s singing voice.//Fews days later Ralia runs away from the witch house.
+// She is taken in by a hunter and his family which Ralia befriends the hunter daughter.
+// One day while playing together, She is knocked down by a prince who takes her his home to nurse her.
+
+//After recovering Ralia is taken back home to her village where she reunites with her parents.
+
+
+let userInputs=[];
+
+// The Plot Ralia Story
+let originalStory = `<h2>Ralia is the ${userInputs[0]},of a blind ${userInputs[1]} and a 
+${userInputs[2]} with a broken back who goes ${userInputs[3]} in the forest while arranging 
+${userInputs[4]} and is taken in by an old ${userInputs[5]} who offer her accommodation in 
+return for Ralia's singing ${userInputs[6]}.Fews days later Ralia runs away from the ${userInputs[5]} house.
+she is taken by a  ${userInputs[7]} and his family which Ralia befriends the ${userInputs[7]},${userInputs[0]}.
+"</h2>
+ <h2>One day while"${userInputs[8]} together" she is knocked down by the  ${userInputs[9]},who takes her to his home to nurse her.
+  After recovering ${userInputs[10]}  was taken back home to her ${userInputs[11]} where she unites with her parents"</h2>`;
+console.log(originalStory);
+
